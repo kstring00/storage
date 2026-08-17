4aa559caa8751aa37874cc372f41ec461c860599
kstring00 <106356792+kstring00@users.noreply.github.com>
2026-08-17 11:28:24 -0500

    Allow Google Analytics through content security policy

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self' https://*.clarity.ms https://c.bing.com; img-src 'self' https://uploads.website.storedge.com https://*.clarity.ms https://c.bing.com data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; script-src 'self' 'unsafe-inline' https://*.clarity.ms; connect-src 'self' https://*.clarity.ms https://c.bing.com; font-src 'self' https://fonts.gstatic.com data:; frame-src https://www.google.com https://maps.google.com; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none'; upgrade-insecure-requests",
+              "default-src 'self' https://*.clarity.ms https://c.bing.com; img-src 'self' https://uploads.website.storedge.com https://*.clarity.ms https://c.bing.com https://*.google-analytics.com https://www.googletagmanager.com data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; script-src 'self' 'unsafe-inline' https://*.clarity.ms https://www.googletagmanager.com; connect-src 'self' https://*.clarity.ms https://c.bing.com https://*.google-analytics.com https://www.googletagmanager.com; font-src 'self' https://fonts.gstatic.com data:; frame-src https://www.google.com https://maps.google.com; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none'; upgrade-insecure-requests",
           },
         ],
       },
