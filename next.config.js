b696a06527d1c9d74f7564af6bb0d328369b8ff7
kstring00 <106356792+kstring00@users.noreply.github.com>
2026-08-17 10:30:04 -0500

    Allow embedded maps in site CSP

--- next.config.js
+++ next.config.js
@@ -41,7 +41,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; img-src 'self' https://uploads.website.storedge.com data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; connect-src 'self'; font-src 'self' data:; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none'; upgrade-insecure-requests",
+              "default-src 'self'; img-src 'self' https://uploads.website.storedge.com data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; connect-src 'self'; font-src 'self' data:; frame-src https://www.google.com https://maps.google.com; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none'; upgrade-insecure-requests",
           },
         ],
       },
