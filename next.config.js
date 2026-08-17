6b95f24f738216acb6872db0ab91e4fa12214a24
kstring00 <106356792+kstring00@users.noreply.github.com>
2026-08-16 22:12:26 -0500

    Optimize remote facility images through Next.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploads.website.storedge.com",
+      },
+    ],
+    minimumCacheTTL: 86400,
+  },
   async headers() {
     return [
       {
